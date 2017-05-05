8f891202d2ae70cace9714e237bb4553d9d4b779
215010 <215010@CPU02000835>
2017-05-04 21:38:11 -0300

    Models

---
+++ SistemaFilmes/Models/Artista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    public class Artista
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public DateTime DtNascto { get; set; }
+        public string Naturalidade { get; set; }
+        public byte Imagem  { get; set; }
+    }
+}
---
+++ SistemaFilmes/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    public class Item
+    {
+        public int Codigo { get; set; }
+        public int CodigoDeBarras { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        public string Tipo { get; set; }
+        public Decimal  Preco { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public Decimal VlCusto { get; set; }
+        public bool Situacao { get; set; }
+        public string  Diretor { get; set; }
+        public byte Imagem { get; set; }
+    }
+}
