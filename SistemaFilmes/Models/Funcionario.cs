62ebbf40a2d07b61220f13b7b9142bdb1b39b1a3
215010 <215010@CPU02000835>
2017-05-04 21:15:49 -0300

    Criando Models

---
+++ SistemaFilmes/Models/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    class Funcionario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public DateTime DtNascto { get; set; }
+        public char Sexo { get; set; }
+        public string EstadoCivil { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public string CEP { get; set; }
+        public string UF { get; set; }
+        public string TEL { get; set; }
+        public string CEL { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+    }
+}
