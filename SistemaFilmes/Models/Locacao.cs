0b97cb053e9ec9b1482da0c4089e9aa91414d3dd
215010 <215010@CPU02000835>
2017-05-11 22:17:42 -0300

    Locacao/PARCIAL

---
+++ SistemaFilmes/Models/Locacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{ 
+    public class Locacao
+    {
+        public int cdLocacao { get; set; }
+        public int cdFunc { get; set; }
+        public int cdFilme { get; set; }
+        public int cdCli { get; set; }
+        public DateTime dtRetirada { get; set; }
+        public DateTime dtDevolucao { get; set; }
+    }
+}
--- SistemaFilmes/SistemaFilmes/frmLocacao.cs
+++ SistemaFilmes/SistemaFilmes/frmLocacao.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DAL;
+using Models;
+
 
 namespace SistemaFilmes
 {
@@ -18,6 +21,32 @@ namespace SistemaFilmes
 
         private void frmLocacao_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            LocacaoDAL lDAL = new LocacaoDAL();
+           // ClienteDAL cDAL = new ClienteDAL();
+            Locacao l = new Locacao();
+            Cliente c = new Cliente();
+            l = lDAL.SelecionarLocacaoPeloCodigo(Convert.ToInt32(txtCodFunc.Text));
+            /********************
+             *Busca do Cliente  *
+             *                  *
+             *                  *
+             *                  *
+             ********************/
+            txtCodLocacao.Text = l.cdLocacao.ToString();
+            txtCodFunc.Text = l.cdFunc.ToString();
+            txtCPF.Text = c.CPF.ToString();
+            txtNomeCli.Text = c.Nome.ToString();
+            dtpAtual.Text = l.dtRetirada.ToString();
+
+
+
+
+
 
         }
     }
