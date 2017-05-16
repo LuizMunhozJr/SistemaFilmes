d03ee101d69e63f41ecbc26e1ca1bf71a072e380
215010 <215010@CPU02000835>
2017-05-16 20:34:45 -0300

    Atualizacao Locacao

--- SistemaFilmes/DAL/itemDAL.cs
+++ SistemaFilmes/DAL/itemDAL.cs
@@ -4,11 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
 using Models;
+using System.Configuration;
 
 namespace DAL
 {
     public class itemDAL
     {
+        string connectionString = ConfigurationManager.ConnectionStrings["BDVideolandiaConnectionString"].ConnectionString;
+
+        public List<Item> SelecionarTodosItensPedido(int codigo)
+        {
+            List<Item> lista = new List<Item>();
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+                string sql = "select * from Filmes where cdFilme = (select cdFilme from ItemLocacao where cdLocacao = @codigo)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    Item i;
+                    while (dr.Read())
+                    {
+                        i = new Item();
+                        i.Codigo = Convert.ToInt32(dr["cdFilme"]);
+                        i.CodigoDeBarras = Convert.ToInt32(dr["cddbarFilme"]);
+                        i.Descricao = dr["dsFilme"].ToString();
+                        i.Preco = Convert.ToDecimal(dr["precoFilme"]);
+
+                        lista.Add(i);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                    conn.Close();
+            }
+
+            return lista;
+        }
 
     }
 }
---
+++ SistemaFilmes/DAL/locacaoDAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Models;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace DAL
+{
+    public class locacaoDAL
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["BDVideolandiaConnectionString"].ConnectionString;
+
+        public Locacao SelecionarLocacaoPeloCodigo(int codigo)
+        {
+            Locacao l = null;
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM Locacoes WHERE cdLocacao = @codigo";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows && dr.Read())
+                {
+                    l = new Locacao();
+                    l.cdLocacao = codigo;
+                    l.cdFunc = Convert.ToInt32(dr["cdFunc"]);
+                    l.cdFilme = Convert.ToInt32(dr["cdFilme"]);
+                    l.cdCli = Convert.ToInt32(dr["cdCli"]);
+                    l.dtRetirada = Convert.ToDateTime(dr["dtRetirada"]);
+                    l.dtDevolucao = Convert.ToDateTime(dr["dtDevolucao"]); ;
+                    l.StatusPG = dr["StatusPG"].ToString();
+                    l.qdteFilmesLocados = dr.HasRows
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                    conn.Close();
+            }
+
+            return l;
+        }
+
+
+
+
+    }
+}
