3fc65312f01d05f40f63fdfeb34328ec764e91cc
215010 <215010@CPU02000835>
2017-05-16 22:01:35 -0300

    ArtistaDAL

--- SistemaFilmes/DAL/artistaDAL.cs
+++ SistemaFilmes/DAL/artistaDAL.cs
@@ -23,22 +23,12 @@ namespace DAL
                 {
                     conn.Open();
 
-                    string sql = "INSERT INTO Artistas VALUES (@nmFunc, @dtFunc, @sxFunc, @ecFunc, @rgFunc, @cpfFunc, @endFunc, @cidFunc, @cepFunc, @ufFunc, @telFunc, @celFunc, @emailFunc, @stsFunc)";
+                    string sql = "INSERT INTO Artistas VALUES (@nmArt, @dtArt, @natuArt, @imgArt)";
                     SqlCommand cmd = new SqlCommand(sql, conn);
-                    cmd.Parameters.AddWithValue("@nmFunc", objArtista.Nome);
-                    cmd.Parameters.AddWithValue("@dtFunc", objArtista.DtNascto);
-                    cmd.Parameters.AddWithValue("@sxFunc", objArtista.Sexo);
-                    cmd.Parameters.AddWithValue("@ecFunc", objArtista.EstadoCivil);
-                    cmd.Parameters.AddWithValue("@rgFunc", objArtista.RG);
-                    cmd.Parameters.AddWithValue("@cpfFunc", objArtista.CPF);
-                    cmd.Parameters.AddWithValue("@endFunc", objArtista.Endereco);
-                    cmd.Parameters.AddWithValue("@cidFunc", objArtista.Cidade);
-                    cmd.Parameters.AddWithValue("@cepFunc", objArtista.CEP);
-                    cmd.Parameters.AddWithValue("@ufFunc", objArtista.UF);
-                    cmd.Parameters.AddWithValue("@telFunc", objArtista.TEL);
-                    cmd.Parameters.AddWithValue("@celFunc", objArtista.CEL);
-                    cmd.Parameters.AddWithValue("@emailFunc", objArtista.Email);
-                    cmd.Parameters.AddWithValue("@stsFunc", objArtista.Status);
+                    cmd.Parameters.AddWithValue("@nmArt", objArtista.Nome);
+                    cmd.Parameters.AddWithValue("@dtArt", objArtista.DtNascto);
+                    cmd.Parameters.AddWithValue("@natuArt", objArtista.Naturalidade);
+                    cmd.Parameters.AddWithValue("@imgArt", objArtista.Imagem);
 
                     cmd.ExecuteNonQuery();
 
@@ -72,27 +62,18 @@ namespace DAL
 
                     if (dr.HasRows)
                     {
-                        Artista f = null;
+                        Artista a = null;
                         while (dr.Read())
                         {
-                            f = new Artista();
-                            f.Codigo = Convert.ToInt32(dr["CdArtista"]);
-                            f.Nome = dr["nmFunc"].ToString();
-                            f.DtNascto = Convert.ToDateTime(dr["dtFunc"]);
-                            f.Sexo = dr["sxFunc"].ToString();
-                            f.EstadoCivil = dr["ecFunc"].ToString();
-                            f.RG = dr["rgFunc"].ToString();
-                            f.CPF = dr["cpfFunc"].ToString();
-                            f.Endereco = dr["endFunc"].ToString();
-                            f.Cidade = dr["cidFunc"].ToString();
-                            f.CEP = dr["cepFunc"].ToString();
-                            f.UF = dr["ufFunc"].ToString();
-                            f.TEL = dr["telFunc"].ToString();
-                            f.CEL = dr["celFunc"].ToString();
-                            f.Email = dr["emailFunc"].ToString();
-                            f.Status = Convert.ToBoolean(dr["stsFunc"]);
+                            a = new Artista();
+                            a.Codigo = Convert.ToInt32(dr["cdArt"]);
+                            a.Nome = dr["nmArt"].ToString();
+                            a.DtNascto = Convert.ToDateTime(dr["dtArt"]);
+                            a.Naturalidade = dr["natuArt"].ToString();
+                            a.Imagem = Convert.ToByte(dr["imgArt"]);
+                           
 
-                            lista.Add(f);
+                            lista.Add(a);
                         }
                     }
                 }
@@ -110,10 +91,10 @@ namespace DAL
                 return lista;
             }
 
-            //DONE
+            
             public Artista BuscarArtista(int cod)
             {
-                Artista F = null;
+                Artista a = null;
 
                 SqlConnection conn = new SqlConnection(connectionString);
 
@@ -121,7 +102,7 @@ namespace DAL
                 {
                     conn.Open();
 
-                    string sql = "SELECT * FROM Artistas WHERE cdFunc = @codigo";
+                    string sql = "SELECT * FROM Artistas WHERE cdArt = @codigo";
                     SqlCommand cmd = new SqlCommand(sql, conn);
                     cmd.Parameters.AddWithValue("@codigo", cod);
 
@@ -129,22 +110,13 @@ namespace DAL
 
                     if (dr.HasRows && dr.Read())
                     {
-                        F = new Artista();
-                        F.CEL = dr["celFunc"].ToString();
-                        F.CEP = dr["cepFunc"].ToString();
-                        F.Cidade = dr["cidFunc"].ToString();
-                        F.CPF = dr["cpfFunc"].ToString();
-                        F.DtNascto = Convert.ToDateTime(dr["dtFunc"].ToString());
-                        F.Email = dr["emailFunc"].ToString();
-                        F.Endereco = dr["endFunc"].ToString();
-                        F.EstadoCivil = dr["ecFunc"].ToString();
-                        F.Nome = dr["nmFunc"].ToString();
-                        F.RG = dr["rgFunc"].ToString();
-                        F.Sexo = dr["sxFunc"].ToString();
-                        F.Status = Convert.ToBoolean(dr["stsFunc"].ToString());
-                        F.TEL = dr["telFunc"].ToString();
-                        F.UF = dr["ufFunc"].ToString();
-                    }
+                    a = new Artista();
+                    a.Codigo = Convert.ToInt32(dr["cdArt"]);
+                    a.Nome = dr["nmArt"].ToString();
+                    a.DtNascto = Convert.ToDateTime(dr["dtArt"]);
+                    a.Naturalidade = dr["natuArt"].ToString();
+                    a.Imagem = Convert.ToByte(dr["imgArt"]);
+                }
                 }
                 catch (Exception)
                 {
@@ -156,10 +128,10 @@ namespace DAL
                         conn.Close();
                 }
 
-                return F;
+                return a;
             }
 
-            //DONE
+            
             public void AlteraArtista(Artista objArtista)
             {
                 SqlConnection conn = new SqlConnection(connectionString);
@@ -168,25 +140,15 @@ namespace DAL
                 {
                     conn.Open();
 
-                    string sql = "UPDATE Artistas SET nmFunc=@nmFunc, dtFunc=@dtFunc, sxFunc=@sxFunc, ecFunc=@ecFunc, rgFunc=@rgFunc, cpfFunc=@cpfFunc, endFunc=@endFunc, cidFunc=@cidFunc, cepFunc=@cepFunc, ufFunc=@ufFunc, telFunc=@telFunc, celFunc=@celFunc, emailFunc=@emailFunc, stsFunc=@stsFunc WHERE cdFunc =@cdFunc";
+                    string sql = "UPDATE Artistas SET nmArt=@nmArt, dtArt=@dtArt, natuArt=@natuArt, imgArt=@imgArt";
                     SqlCommand cmd = new SqlCommand(sql, conn);
-                    cmd.Parameters.AddWithValue("@cdFunc", objArtista.Codigo);
-                    cmd.Parameters.AddWithValue("@nmFunc", objArtista.Nome);
-                    cmd.Parameters.AddWithValue("@dtFunc", objArtista.DtNascto);
-                    cmd.Parameters.AddWithValue("@sxFunc", objArtista.Sexo);
-                    cmd.Parameters.AddWithValue("@ecFunc", objArtista.EstadoCivil);
-                    cmd.Parameters.AddWithValue("@rgFunc", objArtista.RG);
-                    cmd.Parameters.AddWithValue("@cpfFunc", objArtista.CPF);
-                    cmd.Parameters.AddWithValue("@endFunc", objArtista.Endereco);
-                    cmd.Parameters.AddWithValue("@cidFunc", objArtista.Cidade);
-                    cmd.Parameters.AddWithValue("@cepFunc", objArtista.CEP);
-                    cmd.Parameters.AddWithValue("@ufFunc", objArtista.UF);
-                    cmd.Parameters.AddWithValue("@telFunc", objArtista.TEL);
-                    cmd.Parameters.AddWithValue("@celFunc", objArtista.CEL);
-                    cmd.Parameters.AddWithValue("@emailFunc", objArtista.Email);
-                    cmd.Parameters.AddWithValue("@stsFunc", objArtista.Status);
+                    cmd.Parameters.AddWithValue("@nmArt", objArtista.Nome);
+                    cmd.Parameters.AddWithValue("@dtArt", objArtista.DtNascto);
+                    cmd.Parameters.AddWithValue("@natuArt", objArtista.Naturalidade);
+                    cmd.Parameters.AddWithValue("@imgArt", objArtista.Imagem);
+
                     cmd.ExecuteNonQuery();
-                }
+            }
                 catch (Exception)
                 {
                     throw;
@@ -198,7 +160,7 @@ namespace DAL
                 }
             }
 
-            //DONE
+            
             public void ExcluirArtista(int codigo)
             {
                 SqlConnection conn = new SqlConnection(connectionString);
@@ -207,7 +169,7 @@ namespace DAL
                 {
                     conn.Open();
 
-                    string sql = "DELETE FROM Artistas WHERE cdFunc = @codigo";
+                    string sql = "DELETE FROM Artistas WHERE cdArt = @codigo";
                     SqlCommand cmd = new SqlCommand(sql, conn);
                     cmd.Parameters.AddWithValue("@codigo", codigo);
 
@@ -225,5 +187,5 @@ namespace DAL
                 }
             }
         }
-    }
 }
+
