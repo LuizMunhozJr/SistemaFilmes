496c876e84bb11144f8cebddcdae23eb29e135fc
CPD02 <CPD02@NOTE_CPD_02>
2017-05-23 15:57:27 -0300

    Login usuario

---
+++ SistemaFilmes/DAL/usuarioDAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Models;
+
+namespace DAL
+{
+    public class usuarioDAL
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["BDVideolandiaConnectionString"].ConnectionString;
+
+        public static string CalculaSHA1(string text)
+        {
+            try
+            {
+                byte[] buffer = Encoding.Default.GetBytes(text);
+                System.Security.Cryptography.SHA1CryptoServiceProvider cryptoTransformSHA1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+                string hash = BitConverter.ToString(cryptoTransformSHA1.ComputeHash(buffer)).Replace("-", "");
+                return hash;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool  AutenticaUsuario(string Usuario, string senha)
+        {
+            bool Autentica = false;
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM Usuarios WHERE NomeUsuario = @Usuario AND SenhaUs = @senha";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Usuario", Usuario);
+                cmd.Parameters.AddWithValue("@SenhaUs", senha);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    Autentica = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                    conn.Close();
+            }
+
+            return Autentica;
+        }
+    }
+}
---
+++ SistemaFilmes/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        public string NomeUsuario { get; set; }
+
+        public string SenhaUs { get; set; }
+    }
+}
